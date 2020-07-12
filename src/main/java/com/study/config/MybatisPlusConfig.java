2895100f08b9b0ce46866d534d2ff5adaeaac601
ding <982352391@qq.com>
2020-07-12 19:04:04 +0800

    MybatisPlus配置类

---
+++ src/main/java/com/study/config/MybatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.study.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MybatisPlus配置类
+ * @author Deja wu
+ */
+@Configuration
+@ConditionalOnClass(value = {PaginationInterceptor.class})
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        return new PaginationInterceptor();
+    }
+}
